e6fd04ea7243bb6fd0e8a64fc934e6069090c731
avi-11 <singh.avi1109@gmail.com>
2023-07-31 20:08:36 +0530

    -- initial commit --

---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+const ChatPage = () => {
+
+  const [chats,setChats]=useState([]);
+
+  const getChats=async ()=>{
+    const {data}=await axios.get("/api/chat");
+    setChats(data);
+  }
+
+  useEffect(()=>{
+    getChats();
+  },[]);
+
+  return (
+    <div>
+      {chats.map((chat)=>(
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
+    </div>
+  )
+}
+
+export default ChatPage
