6c87666e538cf370e44895012a5fe42ea4b1285d
avi-11 <singh.avi1109@gmail.com>
2023-08-05 17:25:19 +0530

    -- chat bar --

--- frontend/src/components/misc/SideDrawer.js
+++ frontend/src/components/misc/SideDrawer.js
@@ -1,6 +1,6 @@
 import { Button } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
-// import { Input } from "@chakra-ui/input";
+import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import {
   Menu,
@@ -9,27 +9,27 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/menu";
-// import {
-//   Drawer,
-//   DrawerBody,
-//   DrawerContent,
-//   DrawerHeader,
-//   DrawerOverlay,
-// } from "@chakra-ui/modal";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { Avatar } from "@chakra-ui/avatar";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { useToast } from "@chakra-ui/toast";
-// import ChatLoading from "../ChatLoading";
-// import { Spinner } from "@chakra-ui/spinner";
+import ChatLoading from "../ChatLoading";
+import { Spinner } from "@chakra-ui/spinner";
 import ProfileModal from "./ProfileModal";
 // import NotificationBadge from "react-notification-badge";
 // import { Effect } from "react-notification-badge";
-// import { getSender } from "../../config/ChatLogics";
-// import UserListItem from "../userAvatar/UserListItem";
+import { getSender } from "../../config/ChatLogic";
+import UserListItem from "../userAvatar/UserListItem";
 import { ChatState } from '../../context/ChatProvider';
 
 function SideDrawer() {
@@ -39,7 +39,9 @@ function SideDrawer() {
   const [loadingChat, setLoadingChat] = useState(false);
 
   const {
-    user,
+    user,setSelectedChat,
+    chats,
+    setChats,
   } = ChatState();
 
   const toast = useToast();
@@ -51,71 +53,71 @@ function SideDrawer() {
     navigate("/");
   };
 
-//   const handleSearch = async () => {
-//     if (!search) {
-//       toast({
-//         title: "Please Enter something in search",
-//         status: "warning",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "top-left",
-//       });
-//       return;
-//     }
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
 
-//     try {
-//       setLoading(true);
+    try {
+      setLoading(true);
 
-//       const config = {
-//         headers: {
-//           Authorization: `Bearer ${user.token}`,
-//         },
-//       };
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
 
-//       const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`/user?search=${search}`, config);
 
-//       setLoading(false);
-//       setSearchResult(data);
-//     } catch (error) {
-//       toast({
-//         title: "Error Occured!",
-//         description: "Failed to Load the Search Results",
-//         status: "error",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom-left",
-//       });
-//     }
-//   };
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
-//   const accessChat = async (userId) => {
-//     console.log(userId);
+  const accessChat = async (userId) => {
+    console.log(userId);
 
-//     try {
-//       setLoadingChat(true);
-//       const config = {
-//         headers: {
-//           "Content-type": "application/json",
-//           Authorization: `Bearer ${user.token}`,
-//         },
-//       };
-//       const { data } = await axios.post(`/api/chat`, { userId }, config);
+    try {
+      setLoadingChat(true);
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.post(`/chat`, { userId }, config);
 
-//       if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
-//       setSelectedChat(data);
-//       setLoadingChat(false);
-//       onClose();
-//     } catch (error) {
-//       toast({
-//         title: "Error fetching the chat",
-//         description: error.message,
-//         status: "error",
-//         duration: 5000,
-//         isClosable: true,
-//         position: "bottom-left",
-//       });
-//     }
-//   };
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      setSelectedChat(data);
+      setLoadingChat(false);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
   return (
     <>
@@ -185,12 +187,12 @@ function SideDrawer() {
         </div>
       </Box>
 
-      {/* <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
           <DrawerBody>
-            <Box d="flex" pb={2}>
+            <Box display="flex" pb={2}>
               <Input
                 placeholder="Search by name or email"
                 mr={2}
@@ -213,7 +215,7 @@ function SideDrawer() {
             {loadingChat && <Spinner ml="auto" d="flex" />}
           </DrawerBody>
         </DrawerContent>
-      </Drawer> */}
+      </Drawer>
     </>
   );
 }
