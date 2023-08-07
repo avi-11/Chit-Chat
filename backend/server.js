65e1e6d8266cfe5f84d85e8053ba72c4a8ae1636
avi-11 <singh.avi1109@gmail.com>
2023-08-07 13:31:23 +0530

    -- deploy --

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/userRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const chatRouter = require('./routes/chatRoutes');
 const messageRouter = require('./routes/messageRoutes');
+const path=require('path')
 
 const app=express();
 app.use(express.json())
@@ -25,8 +26,28 @@ app.use('/user',userRouter);
 app.use('/chat',chatRouter);
 app.use('/message',messageRouter)
 
+
+// --------- deployment
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// ---------------
+
 app.use(notFound);
 app.use(errorHandler)
+
 
 const PORT=process.env.PORT|| 5000;
 
