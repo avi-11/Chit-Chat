b73d06645e04ab08773e202b4f88ebb672d3b3db
avi-11 <singh.avi1109@gmail.com>
2023-10-02 22:47:18 +0530

    -- context updated --

--- frontend/src/context/ChatProvider.js
+++ frontend/src/context/ChatProvider.js
@@ -1,44 +1,33 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import {createContext, useContext, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ChatContext = createContext();
+const ChatContext=createContext();
 
-const ChatProvider = ({ children }) => {
-  const [selectedChat, setSelectedChat] = useState();
-  const [user, setUser] = useState();
-  const [notification, setNotification] = useState([]);
-  const [chats, setChats] = useState();
+const ChatProvider=({children})=>{
+    const [user,setUser]=useState()
+    const [selectedChat,setSelectedChat]=useState();
+    const [chats,setChats]=useState([]);
+    const [notification,setNotification]=useState([])
+    const navigate=useNavigate()
 
-  const history = useHistory();
+    useEffect(()=>{
+        const userInfo=JSON.parse(localStorage.getItem("userInfo"));
+        setUser(userInfo);
 
-  useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    setUser(userInfo);
+        if(!userInfo){
+            navigate('/');
+        }
+    },[navigate])
 
-    if (!userInfo) history.push("/");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [history]);
+    return(
+        <ChatContext.Provider value={{user,setUser,selectedChat,setSelectedChat,chats,setChats,notification,setNotification}}>
+            {children}
+        </ChatContext.Provider>
+    );
+}
 
-  return (
-    <ChatContext.Provider
-      value={{
-        selectedChat,
-        setSelectedChat,
-        user,
-        setUser,
-        notification,
-        setNotification,
-        chats,
-        setChats,
-      }}
-    >
-      {children}
-    </ChatContext.Provider>
-  );
-};
+export const ChatState=()=>{
+    return useContext(ChatContext);
+}
 
-export const ChatState = () => {
-  return useContext(ChatContext);
-};
-
-export default ChatProvider;
+export default ChatProvider
